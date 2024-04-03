16dd03023872a6fa1afb7308219584ded1167c54
mansf-osk <mansfeld.oskar@gmail.com>
2024-04-03 19:25:53 +0200

    feat: implemented adding new trick

--- frontend/src/api/ApiFunctions.js
+++ frontend/src/api/ApiFunctions.js
@@ -15,3 +15,29 @@ export async function apiGetData(endpoint, options) {
     });
 }
 
+export async function apiPostTrick(endpoint, formData) {
+  const apiUrl = url + endpoint;
+  console.log("Initiating API-call to: \n", apiUrl);
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      "name": formData.name,
+      "difficulty": formData.difficulty,
+      "learned": formData.learned,
+      "clip": formData.clip
+    })
+  };
+
+  return fetch(apiUrl, options)
+    .then((response) => {
+      if (response.ok) {
+        console.log(`API call to '${options.method} ${endpoint}' successful`);
+      } else {
+        console.log(`Error during API call to '${options.method} ${endpoint}'`);
+      };
+    });
+}
